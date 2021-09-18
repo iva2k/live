886ccb31950cacddfab8f3214bd20d8cedd7ab05
Ilya <iva2k@yahoo.com>
2021-09-18 13:09:57 -0700

    Improve VSCode setup * Add storybook extensions * Fix Webpack paths

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     'stylelint-config-sass-guidelines',
     // 'stylelint-config-standard',
-    'stylelint-config-prettier',
+    'stylelint-config-prettier', // Must be last
   ],
   ignoreFiles: [
     './coverage/**/*',
